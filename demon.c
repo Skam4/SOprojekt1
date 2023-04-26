84f8fca9a8ecd91c320f3b6bfdbd1f8102253672
SkyBestPL <107127780+SkyBestPL@users.noreply.github.com>
2023-04-27 00:35:17 +0200

    unormalnianie kontynuacja
    - dodano funkcje: jedną wypisującą wynik polecenia oraz drugą wypisującą error (jedna z tych funkcji działa)
    - inne zmiany jak np. kod wyjścia chyba działa

--- demon.c
+++ demon.c
@@ -36,12 +36,29 @@ void Sortowanie(int zadania[][4], int n, char komendy[][100]) {
     }
 }
 
+//wypisanie wyniku polecenia do pliku
+void stdout_plik(int wypisanie, char komendy[][100], int zadanie)
+{
+    dup2(wypisanie, STDOUT_FILENO);
+    close(wypisanie);
+    execlp(komendy[zadanie], komendy[zadanie], NULL);
+    perror("execlp");
+    exit(1);
+}
+
+//wypisanie bledu polecenia do pliku
+void stderr_plik()
+{
+    
+}
+
 int main(int argc, char *argv[]) 
 {
     pid_t pid, sid;
 	
     // Pobieramy taskfile i outfile
-    if(argc != 3) {
+    if(argc != 3) 
+    {
         printf("Za duzo lub za malo wprowadzonych danych!\n");
         return 1;
     }
@@ -71,28 +88,31 @@ int main(int argc, char *argv[])
     int zadania_tab[1000][4]; //tablica z oddzielonymi wartosciami
     char komendy[100][100]; //tablica char do przechowywania komend
 
-    char znak;
-    char odczytany_bajt;
-    int i=0;
+    char znak; //kolejne odczytywane znaki
+    char odczytany_bajt; //pomocnicza zmienna do sprawdzania, czy czytanie nadal trwa
+    int i=0; //pomocnicza zmienna do liczenia ktory z kolei znak wczytujemy
 
-    ////// linia=bufor, 100 = maksymalna dlugosc
     while (odczytany_bajt = read(zadania, &znak, 1) > 0)
     {
         if(znak == '\n')
         {
-            linia[i] = '\0'; // przetwarzanie linii tekstu
+            linia[i] = '\0'; //Przetwarzanie linii tekstu
             i=0;
+
             wartosc = strtok(linia, ":");
             zadania_tab[ilosc_zadan][0] = atoi(wartosc); //atoi zamienia char na int
+            
             wartosc = strtok(NULL, ":");
             zadania_tab[ilosc_zadan][1] = atoi(wartosc);
+            
             wartosc = strtok(NULL, ":");
             zadania_tab[ilosc_zadan][2] = atoi(wartosc);
-            strcpy(komendy[ilosc_zadan], wartosc); //zapisywanie napisu jako char w tablicy komendy, bo wczesniej zamienia sie w int i jest to bez sensu
+            strcpy(komendy[ilosc_zadan], wartosc); //Zapisywanie polecenia jako char w tablicy komend
+            
             wartosc = strtok(NULL, ":");
             zadania_tab[ilosc_zadan][3] = atoi(wartosc);
+            
             ilosc_zadan++;
-            printf("\nilosc_zadan: %d\n",ilosc_zadan);
         }
         else
         {
@@ -103,30 +123,30 @@ int main(int argc, char *argv[])
     //Sortowanie chronologiczne instrukcji
     Sortowanie(zadania_tab, ilosc_zadan, komendy);
     
-    int zadanie = 0;
+    int zadanie = 0; //Oblicza ilosc zrobionych zadan
     int kod_wyjscia = 0;
     
     pid = fork(); //Fork- funkcja uruchamia nowy proces potomny
     
     openlog("Proces potomny", LOG_PID, LOG_USER); //Inicjalizacja log
     
-    if (pid < 0) {
-        printf("Nie udalo sie utworzyc procesu potomnego dla polecenia:"); //Trzeba dodać które
+    if (pid < 0) 
+    {
+        printf("Nie udalo sie utworzyc procesu potomnego");
         exit(EXIT_FAILURE);
     }
-    
-    if (pid == 0) 
+    else if (pid == 0) 
     {
-    	//Musimy sprawdzać czy już jest czas i wtedy lecimy do ifów
+    	//Proces potomny dziala w petli sprawdzajac, czy juz jest czas na wykonanie zadania
     	for(;;)
     	{
-            time_t now = time(NULL); //pobieramy czas z systemu
+            time_t now = time(NULL); //Pobieramy czas z systemu
 	    	struct tm *local = localtime(&now);
 	    	int godzina = local->tm_hour;
 	    	int minuta = local->tm_min;
 	    	if(zadania_tab[zadanie][0] == godzina && zadania_tab[zadanie][1] == minuta)
 	    	{
-                pid_t pid2 = fork(); //proces potomny wykonujacy zadanie
+                pid_t pid2 = fork(); //Proces potomny wykonujacy zadanie
                 if (pid2 < 0) 
 	    	    {
 			        printf("Nie udalo sie utworzyc procesu potomnego dla polecenia %s.", komendy[zadanie]);
@@ -134,35 +154,48 @@ int main(int argc, char *argv[])
 	    	    }
                 else if(pid2==0)
                 {
-                    pid_t pid3 = fork();
+                    pid_t pid3 = fork(); //Proces potomny wywolany w celu wykonania zadania
                     if (pid3 < 0) 
                     {
-                        perror("fork");
-                        exit(1);
+                        printf("Nie udalo sie utworzyc procesu potomnego dla polecenia %s.", komendy[zadanie]);
+			            exit(EXIT_FAILURE);
                     } 
                     else if (pid3 == 0) 
                     {
-                        dup2(wypisanie, STDOUT_FILENO);
-                        close(wypisanie);
-                        execlp(komendy[zadanie], komendy[zadanie], NULL);
-                        perror("execlp");
-                        exit(1);
-                    } 
-                    else 
-                    {
-                        int status;
-                        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
-                        printf("Polecenie wykonane poprawnie.\n");
-                        } else {
-                        printf("Błąd wykonania polecenia.\n");
+                        syslog(LOG_INFO, "Uruchomiono polecenie %s z parametrem %d o godzinie %d:%d\n", komendy[zadanie], zadania_tab[zadanie][3], zadania_tab[zadanie][0], zadania_tab[zadanie][1]);
+
+                        char buf[100];
+                        snprintf(buf, sizeof(buf), "\nUruchomiono polecenie %s z parametrem %d o godzinie %d:%d\n\n", komendy[zadanie], zadania_tab[zadanie][3], zadania_tab[zadanie][0], zadania_tab[zadanie][1]);
+                        write(wypisanie, buf, strlen(buf));
+                        
+                        //Kolejne parametry
+                        if(zadania_tab[zadanie][3] == 0)
+                        {
+                            stdout_plik(wypisanie, komendy, zadanie);
                         }
-                        close(wypisanie);
-                        exit(0);
+                        else if(zadania_tab[zadanie][3] == 1)
+                        {
+                            stderr_plik();
+                        }
+                        else if(zadania_tab[zadanie][3] == 2)
+                        {
+                            stdout_plik(wypisanie, komendy, zadanie);
+                            stderr_plik();
+                        }
+                        else
+                        {
+                            snprintf(buf, sizeof(buf), "\nNieprawidlowa wartosc parametru\n\n");
+                            write(wypisanie, buf, strlen(buf));
+                        }
                     }
+                    kod_wyjscia = close(wypisanie);
+                    syslog(LOG_INFO, "Zakonczono polecenie %s z parametrem %d o godzinie %d:%d z kodem wyjscia %d\n", komendy[zadanie],  zadania_tab[zadanie][3], zadania_tab[zadanie][0], zadania_tab[zadanie][1], kod_wyjscia);
+                    exit(EXIT_SUCCESS);
                 }
                 zadanie++;
                 if(zadanie==ilosc_zadan)
                 {
+                    //zamkniecie pliku dodac
                     exit(EXIT_SUCCESS);
                 }
             }

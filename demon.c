5652410d6a01c1382f7e05eed4628bc8919b7313
SkyBestPL <107127780+SkyBestPL@users.noreply.github.com>
2023-04-28 08:41:27 +0200

    Update demon.c
    Dodanie komentarzy

--- demon.c
+++ demon.c
@@ -17,7 +17,7 @@ void Sortowanie(int zadania[][4], int n, char komendy[][100]) {
     char pom2[100][100];
     for (i = 0; i < n; i++) {
         j = i - 1;
-        //Kopiowanie i-tego wiersza do zmiennej pom
+        //Kopiowanie i-tego wiersza do tablicy pomocniczej
         for (k = 0; k < 4; k++) {
             pom[k] = zadania[i][k];
         }
@@ -27,7 +27,7 @@ void Sortowanie(int zadania[][4], int n, char komendy[][100]) {
             for (k = 0; k < 4; k++) {
                 zadania[j + 1][k] = zadania[j][k];
             }
-            strcpy(komendy[j + 1], komendy[j]); //Te same operacje wykonywane są na tablicy komend, która pr
+            strcpy(komendy[j + 1], komendy[j]); //Te same operacje wykonywane są na tablicy komend, która przetrzymuje kolejne parametry <command>
             j--;
         }
         // wstawienie i-tego elementu w odpowiednie miejsce
@@ -50,7 +50,7 @@ void stdout_stderr(int wypisanie, char komendy[][100], int zadanie, int parametr
     if(parametr==1)
     {
         dup2(dev_null, STDOUT_FILENO); //Przekierowanie standardowego wyjścia do urządzenia /dev/null
-        dup2(wypisanie, STDERR_FILENO); //Przekierowanie wyjścia błędu do urządzenia /dev/null
+        dup2(wypisanie, STDERR_FILENO); //Przekierowanie wyjścia błędu na wypisywanie do pliku
     }
     if(parametr==2)
     {
@@ -81,51 +81,50 @@ void stdout_stderr(int wypisanie, char komendy[][100], int zadanie, int parametr
     exit(1);
 }
 
-//Funkcja zwracająca czas do zadania w sekundach
+//Funkcja zwracająca czas pozostały do wykonania zadania w sekundach
 int CzasDoZadania(int hour, int minutes)
 {
-	time_t now = time(NULL); //pobieramy czas z systemu
-	struct tm *local = localtime(&now);
-	int godzina = local->tm_hour;
-	int minuta = local->tm_min;
-	int czas1, czas2;
-	czas1 = hour*3600 + minutes*60;
+	time_t now = time(NULL); //Pobranie aktualnego czasu z systemu
+	struct tm *local = localtime(&now); //Zastosowanie funkcji localtime do konwertowania wartości now
+	int godzina = local->tm_hour; //Pobranie wartości aktualnej godziny
+	int minuta = local->tm_min; //Pobranie wartości aktualnej ilości minut
+	int czas1, czas2; //Zmienne pomocnocze do liczenia czasu pozostałego do kolejnego wykonywanego zadania
+	czas1 = hour*3600 + minutes*60; //Przemnożenie wartości godzin i minut do wartości sekund
 	czas2 = godzina*3600 + minuta*60;
-	return czas1-czas2 > 1 ? czas1-czas2 : 1;
+	return czas1-czas2 > 1 ? czas1-czas2 : 1; //Jeżeli ilość pozostałych sekund jest większa od 1, to jest przekazywana, a jeżeli jest mniejsza lub róna 1, to zwracana jest 1 sekunda (co w praktyce oznacza, że w tym momencie jest godzina o której należy wykonać kolejne zadanie)
 }
 
-jmp_buf restart_point;
+jmp_buf restart_point; //Punkt skoku wykorzystywany przy wykryciu sygnału SIGUSR1
 
-int **tasks; //tablica z zadaniami, potrzebna do sygnałów
-char *taski; //tablica przetrzymująca polecenia jako napisy
-int wiersze; //Funkcja zapamiętująca ilość wierszy w tablicy tasks
+int **tasks; //Tablica z zadaniami, potrzebna do obsługi sygnałów
+char *taski; //Tablica przetrzymująca polecenia jako napisy
+int wiersze; //Tunkcja zapamiętująca ilość wierszy w tablicy tasks
 
 
-//Funkcja obsługująca sygnał SIGINT
+//Funkcja obsługująca sygnał SIGINT (Ctrl+C)
 void sigint_handler(int sig)
 {
-
-    exit(EXIT_SUCCESS);
+    exit(EXIT_SUCCESS); //W momencie wykrycia sygnału, proces kończy się
 }
 
 //Funkcja obsługująca sygnał SIGUSR1
 void sigusr1_handler(int sig) 
 {
-    // ustawienie wartości powrotu dla longjmp()
+    //Ustawienie wartości powrotu dla longjmp()
     int return_value = 1;
-    // wywołanie longjmp() z przygotowanym punktem skoku
+    //Wywołanie longjmp() z przygotowanym punktem skoku
     longjmp(restart_point, return_value);
 }
 
 //Funkcja obsługująca sygnał SIGUSR2
 void sigusr2_handler(int sig)
 {
-    syslog(LOG_INFO, "Lista zadań pozostala do wykonania:\n");
+    syslog(LOG_INFO, "Lista zadań pozostala do wykonania:\n"); //Wypisanie w logach ilości pozostałych zadań
     for(int i=0 ; i < wiersze ; i++)
     {
-        if(CzasDoZadania(tasks[i][0], tasks[i][1]) >= 1)
+        if(CzasDoZadania(tasks[i][0], tasks[i][1]) >= 1) //Wykorzystanie funkcji CzasDoZadania w celu wypisania pozostałych zadań
         {
-            syslog(LOG_INFO, "Polecenie %s o godzinie %d:%d\n", taski[i], tasks[i][0], tasks[i][1]);
+            syslog(LOG_INFO, "Polecenie %s o godzinie %d:%d\n", taski[i], tasks[i][0], tasks[i][1]); //Wypisywanie w logach systemowych kolejnych pozostałych zadań
         }
     }
 }
@@ -144,16 +143,16 @@ void czysc()
 //Funkcja main
 int main(int argc, char *argv[]) 
 {
-    //if do którego funkcja wchodzi, gdy wywołamy SIGUSR1
+    //If do którego funkcja wchodzi, gdy wywołamy SIGUSR1
     if (setjmp(restart_point) != 0) {
-        // kod do wykonania w momencie powrotu z sigusr1_handler
+        //Kod do wykonania w momencie powrotu z sigusr1_handler
         printf("SIGUSR1 WYKONANY!\n");
     }
 
-    //Tworzymy identyfikator
+    //Tworzenie identyfikatora
     pid_t pid;
 	
-    // Pobieramy taskfile i outfile
+    //Pobieranie taskfile i outfile
     if(argc != 3) 
     {
         printf("Za duzo lub za malo wprowadzonych danych!\n");
@@ -165,7 +164,7 @@ int main(int argc, char *argv[])
     //Do outfile przydzielamy argument 2
     char *outfile = argv[2];
     
-    //otwieranie pliku taskfile i outfile
+    //Otwieranie pliku taskfile i outfile
     int zadania = open(taskfile, O_RDONLY);
     int wypisanie = open(outfile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     
@@ -185,91 +184,82 @@ int main(int argc, char *argv[])
     }
     
     //Wczytywanie danych z pliku taskfile
-    char linia[100]; //bufor
-    char *wartosc; //pomocniczo zapisujemy poszczegolne wartosci
-    int ilosc_zadan = 0; //pomocniczo liczymy ilosc zadan
-    int zadania_tab[1000][4]; //tablica z oddzielonymi wartosciami
-    char komendy[100][100]; //tablica char do przechowywania komend
+    char linia[100]; //Bufor
+    char *wartosc; //pomocniczo zapisujemy kolejne wczytywane wartosci
+    int ilosc_zadan = 0; //pomocniczo liczymy ilosc zadan do wykonania
+    int zadania_tab[1000][4]; //Tablica z oddzielonymi wartosciami (wczytuje wszystkie parametry i dzieli je według dwukropków)
+    char komendy[100][100]; //Tablica char do przechowywania wczytywanych komend (we wczytywaniu parametrów wczytuje parametr <command>)
 
-    char znak; //kolejne odczytywane znaki
+    char znak; //Kolejne odczytywane znaki
     char odczytany_bajt; //pomocnicza zmienna do sprawdzania, czy czytanie nadal trwa
     int i=0; //pomocnicza zmienna do liczenia ktory z kolei znak wczytujemy
 
-    while (odczytany_bajt = read(zadania, &znak, 1) > 0)
+    while (odczytany_bajt = read(zadania, &znak, 1) > 0) //Wczytywanie kolejnych bajtów dopóki nie skończy się plik
     {
-        if(znak == '\n')
+        if(znak == '\n') //Jeżeli wykryto znak "\n", to znaczy że wiersz się kończy i można go zapisywać do tablicy
         {
-            linia[i] = '\0'; //Przetwarzanie linii tekstu
-            i=0;
+            linia[i] = '\0'; //Wczytana linia tekstu zakończona jest znakiem '\0'
+            i=0; //Zerujemy zmienną 'i', aby można jej było użyć przy kolejnym przejściu pętli
 
-            wartosc = strtok(linia, ":");
-            zadania_tab[ilosc_zadan][0] = atoi(wartosc); //atoi zamienia char na int
+            wartosc = strtok(linia, ":"); //Wczytywanie pierwszego parametru; Funkcja strtok wczytuje tekst do momentu napotkania dwukropka
+            zadania_tab[ilosc_zadan][0] = atoi(wartosc); //Atoi zamienia char na int; Pod zerowy indeks przypisujemy parametr pierwszy - <hour>
             
-            wartosc = strtok(NULL, ":");
-            zadania_tab[ilosc_zadan][1] = atoi(wartosc);
+            wartosc = strtok(NULL, ":"); //Wczytywanie drugiego parametru
+            zadania_tab[ilosc_zadan][1] = atoi(wartosc); //Pod pierwszy indeks przypisujemy parametr drugi - <minutes>
             
-            wartosc = strtok(NULL, ":");
-            zadania_tab[ilosc_zadan][2] = atoi(wartosc);
-            strcpy(komendy[ilosc_zadan], wartosc); //Zapisywanie polecenia jako char w tablicy komend
+            wartosc = strtok(NULL, ":"); //Wczytywanie trzeciego parametru
+            zadania_tab[ilosc_zadan][2] = atoi(wartosc); //Pod drugi indeks przypisujemy parametr trzeci - <command>
+            //Ten sam parametr przypisujemy też do tablicy komend w celu zapewnienia poprawnych wartości
+            strcpy(komendy[ilosc_zadan], wartosc); //Kopiowanie wartości trzeciego parametru do tablicy komend za pomocą funkjci strcpy
             
-            wartosc = strtok(NULL, ":");
-            zadania_tab[ilosc_zadan][3] = atoi(wartosc);
+            wartosc = strtok(NULL, ":"); //Wczytywanie czwartego parametru
+            zadania_tab[ilosc_zadan][3] = atoi(wartosc); //Pod trzeci indeks przypisujemy parametr czwarty - <info>
             
-            ilosc_zadan++;
+            ilosc_zadan++; //Zwiększamy wartość zmiennej w celu wczytania kolejnego zestawu parametrów zadania do wykonania
         }
         else
         {
-            linia[i++] = znak;
+            linia[i++] = znak; //Dla kolejnych wartości zmiennej 'i' przypisujemy kolejne wczytywane znaki dla tablicy przechowującej kolejne wczytywane linie z pliku
         }
     }
 
-    close(zadania); //
+    close(zadania); //Zamknięcie pliku taskfile po wczytaniu wszystkich zadań
 
+    //Filtrowanie zadań (pozbywanie się zadań, które w pliku zostały wpisane z godziną wcześniejszą niż godzina rozpoczęcia programu)
     time_t now = time(NULL); //pobieramy czas z systemu
-	struct tm *local = localtime(&now);
-	int godzina = local->tm_hour;
-	int minuta = local->tm_min;
+	struct tm *local = localtime(&now); //Zastosowanie funkcji localtime do konwertowania wartości now
+	int godzina = local->tm_hour; //Pobranie wartości aktualnej godziny
+	int minuta = local->tm_min; //Pobranie wartości aktualnej ilości minut
 
-    //tutaj trzeba zrobić prawdziwą tablice zadania_tab
-    int zadania_tab2[1000][4]; //mozna inaczej nazwac
-    char komendy2[1000][100];
-    int pomoc = 0;
+    int zadania_tab2[1000][4]; //Tablica zadań, która bedzie przechowywała przefiltrowane zadania
+    char komendy2[1000][100]; //Tablica komend przechwująca przefiltrowane komendy
+    int pomoc = 0;//Zmienna pomocnicza oznacza kolejne indeksy zadania_tab2
 
-    for(int i = 0; i < ilosc_zadan ; i++)
+    for(int i = 0; i < ilosc_zadan ; i++) //Przechodzimy po wszystkich wczytanych zadaniach
     {
+        //Jeśli czas wczytanego zadania jest późniejszy lub równy aktualnemu czasowi, to zadanie jest dodawane do tablicy zadania_tab2 na pozycji pomoc
         if(zadania_tab[i][0] > godzina || (zadania_tab[i][0] == godzina && zadania_tab[i][1] >= minuta))
         {
-            zadania_tab2[pomoc][0] = zadania_tab[i][0];
+            zadania_tab2[pomoc][0] = zadania_tab[i][0]; //Przypisywanie kolejnych parametrów do nowej, przefiltrowanej tablicy
             zadania_tab2[pomoc][1] = zadania_tab[i][1];
             zadania_tab2[pomoc][2] = zadania_tab[i][2]; 
             strcpy(komendy2[pomoc], komendy[i]);
             zadania_tab2[pomoc][3] = zadania_tab[i][3];
 
-            printf("zadania_tab[0]: %d     godzina: %d     zadania_tab[1]: %d     minuta: %d\n", zadania_tab[i][0], godzina, zadania_tab[i][1], minuta);
-            printf("pomoc: %d   i: %d\n", pomoc, i);
-            printf("%d : %d\n", zadania_tab2[pomoc][0], zadania_tab2[pomoc][1]);
-
-            pomoc++;
+            pomoc++; //Zwiększenie wartości pomocniczej zmiennej oznaczającej kolejne indeksy zadania_tab2
         }
     }
+    pomoc--; //Jednorazowe zmniejszenie wartości nowej ilości zadań
 
-    pomoc--;
-
-    for(int i=0 ; i<pomoc ; i++)
-    {
-        printf("godzina: %d      minuta: %d\n", zadania_tab2[i][0], zadania_tab2[i][1]);
-    }
-
-
-    //alokacja pamięci tablicy tasks
+    //Alokacja pamięci tablicy tasks
     tasks = (int **) malloc(pomoc+1 * sizeof(int *));
     
-    //alokacja pamięci tablicy taski
+    //Alokacja pamięci tablicy taski
     taski = (char *) malloc(pomoc+1 * sizeof(char));
 
     //wiersze = pomoc;
 
-    // przypisanie zadania_tab2 pod tasks
+    //Przypisanie kolejnych wartości zadania_tab2 pod tasks
     for (int i = 0; i <= pomoc; i++) 
     {
         tasks[i] = (int *) malloc(4 * sizeof(int));
@@ -277,14 +267,14 @@ int main(int argc, char *argv[])
         taski[i] = komendy2[i];
     }
 
-    //Sortowanie chronologiczne instrukcji
+    //Sortowanie chronologiczne instrukcji (wczytujemy tablicę ze wszystkimi parametrami, ilość wszystkich wczytanych zadań oraz tablicę z konkretnymi komendami)
     Sortowanie(*zadania_tab2, pomoc, *komendy2);
     
-    int zadanie = 0; //odlicza ilość zrobionych zadań
-    int kod_wyjscia = 0;
-    int sekundy = 0; //pobiera za ile sekund musi się obudzić demon
+    int zadanie = 0; //Oblicza ilość zrobionych zadań
+    int kod_wyjscia = 0; //Pobiera kod wyjścia
+    int sekundy = 0; //Pobiera za ile sekund musi się obudzić demon
     
-    pid = fork(); //Fork- funkcja uruchamia nowy proces potomny
+    pid = fork(); //Funkcja fork uruchamia nowy proces potomny
     
     openlog("Proces potomny", LOG_PID, LOG_USER); //Inicjalizacja log
 
@@ -309,63 +299,67 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
     
-    //Przypadek błędnego utworzenia procesu
-    if (pid < 0) 
+    if (pid < 0) //Przypadek błędnego utworzenia procesu
     {
         printf("Nie udalo sie utworzyc procesu potomnego");
         exit(EXIT_FAILURE);
     }
     else if (pid == 0) 
     {
-
-    	//Proces dziala w petli sprawdzajac, czy juz jest czas na wykonanie zadania
+    	//Proces działa w pętli sprawdzając, czy juz jest czas na wykonanie zadania
     	while(pomoc >= zadanie)
     	{
+            //Obliczenie ilości sekund pozostałych do wykonania kolejnego zadania
             sekundy = CzasDoZadania(zadania_tab2[zadanie][0], zadania_tab2[zadanie][1]);
-	    	printf("Demon obudzi sie za: %d sekund\n", sekundy);
-	    	sleep(sekundy);
+	    	//printf("Demon obudzi sie za: %d sekund\n", sekundy);
+	    	sleep(sekundy); //Demon śpi przez obliczoną ilość sekund, dzięki czemu budzi się o czasie wykonywania zadania określonym w taskfile
 
-            pid_t pid2 = fork(); //Proces potomny wykonujacy zadanie
-            if (pid2 < 0) 
+            pid_t pid2 = fork(); //Proces potomny wykonujący zadanie
+
+            if (pid2 < 0) //Przypadek błędnego utworzenia procesu
 	    	{
 			    printf("Nie udalo sie utworzyc procesu potomnego dla polecenia %s.", komendy2[zadanie]);
 			    exit(EXIT_FAILURE);
 	    	}
-            else if(pid2==0)
+            else if(pid2==0) //W przypadku sukcesu utworzenia procesu potomnego
             {
+                //Wpisanie do logów i do pliku informacji o wykonywanym zadaniu
                 syslog(LOG_INFO, "Uruchomiono polecenie %s z parametrem %d o godzinie %d:%d\n", komendy2[zadanie], zadania_tab2[zadanie][3], zadania_tab2[zadanie][0], zadania_tab2[zadanie][1]);
-
                 char buf[100];
                 snprintf(buf, sizeof(buf), "\nUruchomiono polecenie %s z parametrem %d o godzinie %d:%d\n\n", komendy2[zadanie], zadania_tab2[zadanie][3], zadania_tab2[zadanie][0], zadania_tab2[zadanie][1]);
                 write(wypisanie, buf, strlen(buf));
                         
-                //Kolejne parametry
+                //Warunki dotyczące kolejnych wartości parametru <info>
                 if(zadania_tab2[zadanie][3] == 0)
                 {
+                    //Uruchamiana jest funkcja stdout_stderr z parametrem = 0, co oznacza, że ma wypisać wynik polecenia do pliku
                     stdout_stderr(wypisanie, komendy2, zadanie, 0);
                 }
                 else if(zadania_tab2[zadanie][3] == 1)
                 {
+                    //Uruchamiana jest funkcja stdout_stderr z parametrem = 1, co oznacza, że ma wypisać wynik wyjścia błędów do pliku (jeżeli jakiś błąd wystąpił)
                     stdout_stderr(wypisanie, komendy2, zadanie, 1);
                 }
                 else if(zadania_tab2[zadanie][3] == 2)
                 {
+                    //Uruchamiana jest funkcja stdout_stderr z parametrem = 2, co oznacza, że ma wypisać wynik polecenia i wynik wyjścia błędów do pliku
                     stdout_stderr(wypisanie, komendy2, zadanie, 2);
                 }
                 else
                 {
+                    //Przypadek podania niewłaściwego parametru <info>
                     snprintf(buf, sizeof(buf), "Nieprawidlowa wartosc parametru\n\n");
                     write(wypisanie, buf, strlen(buf));
                 }
                     
-                kod_wyjscia = close(wypisanie);
+                kod_wyjscia = close(wypisanie); //Odczytanie kodu wyjścia ze zmiennej otwierającej plik wyjściowy
+                //Wpisanie do logów informacji o zakończonym zadaniu
                 syslog(LOG_INFO, "Zakonczono polecenie %s z parametrem %d o godzinie %d:%d z kodem wyjscia %d\n", komendy2[zadanie],  zadania_tab2[zadanie][3], zadania_tab2[zadanie][0], zadania_tab2[zadanie][1], kod_wyjscia);
                 exit(EXIT_SUCCESS);
             }
-            zadanie++;
-            if(zadanie==ilosc_zadan)
+            zadanie++; //Zwiększamy zmienną zliczającą ilość wykonanych zadań
+            if(zadanie==pomoc) //Jeżeli wykonaliśmy już ostatnie zadanie to kończymy proces potomny
             {
-                //zamkniecie pliku dodac
                 exit(EXIT_SUCCESS);
             }
         }
